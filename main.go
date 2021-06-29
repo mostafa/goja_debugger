be306592d5acaa5611554b692f031be9429a138d
Mostafa Moradian <mostafamoradian0@gmail.com>
2021-06-29 12:18:51 +0200

    New REPL (#1)
    * REPL based on the new debug API's event loop (channels and commands)
    * Move help and list in the debugger
    * align help
    * Tell users when they execute non existant commands
    * breakpoints
    * QoL improvements
    Co-authored-by: Mihail Stoykov <M.Stoikov@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"sync"
 
 	"github.com/dop251/goja"
 	"github.com/dop251/goja/parser"
 	"github.com/dop251/goja_nodejs/console"
 	"github.com/dop251/goja_nodejs/require"
+)
+
+var (
+	dbg *goja.Debugger
+	wg  sync.WaitGroup
 )
 
 func main() {
@@ -53,18 +61,74 @@ func main() {
 	}
 
 	runtime := goja.New()
+
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// defer wg.Done()
+
+	// var debugger *goja.Debugger
 	if inspect {
-		runtime.EnableDebugMode()
+		dbg = runtime.EnableDebugMode()
 	}
+
+	// go func() {
+	// 	var prev *Break
+	// 	for b := d.Wait(); b != nil; b, prev = d.Wait(), b {
+	// 		if b != prev {
+	// 			fmt.Printf("Break at %s:%d", b.Filename(), b.Line())
+	// 		}
+	// 		fmt.Println("> ")
+	// 		cmd := parseCmdFromStdin()
+	// 		switch cmd.Name {
+	// 		case "cont", "c":
+	// 			d.Continue()
+	// 		case "next", "n":
+	// 			d.Next()
+	// 		case "list", "l":
+	// 			fmt.Println(b.Source())
+	// 		}
+	// 	}
+	// }()
+	// fmt.Println(debugger.Help().Value)
+	// fmt.Println(debugger.List().Value)
 
 	registry := new(require.Registry)
 	registry.Enable(runtime)
 	registry.RegisterNativeModule("console", console.RequireWithPrinter(printer))
 	console.Enable(runtime)
 
-	_, err = runtime.RunScript(filename, string(content))
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(4)
+	wg.Add(2)
+
+	go runtime.RunScript(filename, string(content))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	os.Exit(4)
+	// }
+	printWhyWeAreDebugging := func(b string) {
+		if b == goja.BreakpointActivation {
+			fmt.Println("hit breakpoint")
+		} else {
+			fmt.Println("hit debugger statement")
+		}
 	}
+
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+
+		b, c := dbg.WaitToActivate()
+		printWhyWeAreDebugging(b)
+		for {
+			fmt.Printf("debug[%d]> ", dbg.GetPC())
+			text, _ := reader.ReadString('\n')
+			// convert CRLF to LF
+			text = strings.Replace(text, "\n", "", -1)
+			if !executor(text) {
+				c()
+				b, c = dbg.WaitToActivate()
+				printWhyWeAreDebugging(b)
+			}
+		}
+	}()
+
+	wg.Wait()
 }
---
+++ repl.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/c-bata/go-prompt"
+	"github.com/dop251/goja"
+)
+
+const (
+	GreenColor = "\u001b[32m"
+	GrayColor  = "\u001b[38;5;245m"
+	ResetColor = "\u001b[0m"
+)
+
+type Command struct {
+	Name string
+	Args []string
+}
+
+func parseCmd(in string) (*Command, error) {
+	data := strings.Split(in, " ")
+	if len(data) == 0 {
+		return nil, errors.New("unknown command")
+	}
+	name := data[0]
+	var args []string
+	if len(data) > 1 {
+		args = append(args, data[1:]...)
+	}
+	return &Command{Name: name, Args: args}, nil
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "setBreakpoint, sb", Description: "Set a breakpoint on a given file and line"},
+		{Text: "clearBreakpoint, cb", Description: "Clear a breakpoint on a given file and line"},
+		{Text: "breakpoints", Description: "List all known breakpoints"},
+		{Text: "next, n", Description: "Continue to next line in current file"},
+		{Text: "cont, c", Description: "Resume execution until next debugger line"},
+		{Text: "step, s", Description: "Step into, potentially entering a function (not implemented yet)"},
+		{Text: "out, o", Description: "Step out, leaving the current function (not implemented yet)"},
+		{Text: "exec, e", Description: "Evaluate the expression and print the value"},
+		{Text: "list, l", Description: "Print the source around the current line where execution is currently paused"},
+		{Text: "print, p", Description: "Print the provided variable's value"},
+		{Text: "help, h", Description: "Print this very help message"},
+		{Text: "quit, q", Description: "Exit debugger and quit (Ctrl+C)"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func executor(in string) bool {
+	cmd, err := parseCmd(in)
+	if err != nil {
+		fmt.Println(err)
+		return true
+	}
+
+	var result goja.Result
+	switch cmd.Name {
+	case "setBreakpoint", "sb":
+		if len(cmd.Args) < 2 {
+			fmt.Println("sb filename linenumber")
+			return true
+		}
+		if line, err := strconv.Atoi(cmd.Args[1]); err != nil {
+			fmt.Printf("Cannot convert %s to line number\n", cmd.Args[1])
+		} else {
+			err := dbg.SetBreakpoint(cmd.Args[0], line)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}
+	case "clearBreakpoint", "cb":
+		if len(cmd.Args) < 2 {
+			fmt.Println("cb filename linenumber")
+			return true
+		}
+		if line, err := strconv.Atoi(cmd.Args[1]); err != nil {
+			fmt.Printf("Cannot convert %s to line number\n", cmd.Args[1])
+		} else {
+			err := dbg.ClearBreakpoint(cmd.Args[0], line)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}
+	case "breakpoints":
+		breakpoints, err := dbg.Breakpoints()
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			for _, b := range breakpoints {
+				fmt.Printf("Breakpoint on %s:%d\n", b.Filename, b.Line)
+			}
+		}
+	case "next", "n":
+		result = dbg.Next()
+	case "cont", "continue", "c":
+		//		result = dbg.Continue()
+		return false
+	case "step", "s":
+		result = dbg.StepIn()
+	case "out", "o":
+		result = dbg.StepOut()
+	case "exec", "e":
+		result = dbg.Exec(strings.Join(cmd.Args, ";"))
+	case "print", "p":
+		result = dbg.Print(strings.Join(cmd.Args, ""))
+	case "list", "l":
+		result = dbg.List()
+		if result.Err == nil {
+			lines := result.Value.([]string)
+
+			currentLine := dbg.Line()
+			lineIndex := currentLine - 1
+			var builder strings.Builder
+			for idx, lineContents := range lines {
+				if InBetween(lineIndex, idx-4, idx+4) {
+					lineNumber := idx + 1
+					totalPadding := 6
+					digitCount := CountDigits(lineNumber)
+					if digitCount >= totalPadding {
+						totalPadding = digitCount + 1
+					}
+					if currentLine == lineNumber {
+						padding := strings.Repeat(" ", totalPadding-digitCount)
+						builder.Write([]byte(fmt.Sprintf("%s>%s %d%s%s\n", GreenColor, ResetColor, currentLine, padding, lines[lineIndex])))
+					} else {
+						padding := strings.Repeat(" ", totalPadding-digitCount)
+						builder.Write([]byte(fmt.Sprintf("%s  %d%s%s%s\n", GrayColor, lineNumber, padding, lineContents, ResetColor)))
+					}
+				}
+			}
+			fmt.Println(builder.String())
+		}
+		return true
+	case "help", "h":
+		fmt.Println(help)
+		return true
+	case "quit", "q":
+		os.Exit(0)
+	default:
+		fmt.Printf("Unknown command, `%s`. You can use `h` to print available commands\n", in)
+	}
+
+	prefix := "<" // this should probably be done differently
+	if result.Value != nil {
+		fmt.Printf("%s%s\n", prefix, result.Value)
+	}
+	if result.Err != nil {
+		fmt.Printf("%sError: %s\n", prefix, result.Err)
+	}
+	return true
+}
+
+func InBetween(i, min, max int) bool {
+	if (i >= min) && (i <= max) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func CountDigits(number int) int {
+	if number < 10 {
+		return 1
+	} else {
+		return 1 + CountDigits(number/10)
+	}
+}
+
+type Cmd struct {
+	Name string
+	Args []string
+}
+
+var help = `
+	setBreakpoint, sb        Set a breakpoint on a given file and line
+	clearBreakpoint, cb      Clear a breakpoint on a given file and line
+	breakpoints              List all known breakpoints
+	next, n                  Continue to next line in current file
+	cont, c                  Resume execution until next debugger line
+	step, s                  Step into, potentially entering a function (not implemented yet)
+	out, o                   Step out, leaving the current function (not implemented yet)
+	exec, e                  Evaluate the expression and print the value
+	list, l                  Print the source around the current line where execution is currently paused
+	print, p                 Print the provided variable's value
+	help, h                  Print this very help message
+	quit, q                  Exit debugger and quit (Ctrl+C)
+`[1:] // this removes the first new line
